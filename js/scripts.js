bd7930df81e9a9de51e937d6018e7a0dd875a04c
hamida-mstafa <hamidamstafa@gmail.com>
2018-10-04 16:08:30 +0300

    add new elements in index.html

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// // myFunction(intro(){
+// //  document.getElementById.styles.display="show";
+// // });
+// // $(document).ready(function() {
+// //   $("button#login").click(function(event){
+// //     $("#home").show();
+// //     $("#about").hide();
+// //   })
+// // })
+// $(document).ready(function() {
+// $("#services").hide();
+// $("#contacts").hide();
+//   $("#service").click(function(){
+//     $("#services").show();
+//     $("#about").hide();
+//     $("#contacts").hide();
+//   });
+// });
