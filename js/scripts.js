f97186d8d6dd0f92e03df755a1f2ad294638a0f7
hamida-mstafa <hamidamstafa@gmail.com>
2018-10-05 12:47:41 +0300

    changed about and services to diffrent htmls

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,23 @@
 //     $("#about").hide();
 //     $("#contacts").hide();
 //   });
+//   $("#contact").click(function(){
+//     $("#contacts").show();
+//     $("#about").hide();
+//     $("#services").hide();
 // });
+// });
+
+// function login(){
+// var username= ""
+// var password=""
+// var inputed_username= document.getElementById('field').value;
+// var inputed_password= document.getElementById('field1').value;
+//
+// if((username==inputed_username) &&(password==inputed_password)){
+//   window.open(/home/moringaschoolcom/Documents/Tender-hunt/file.html);
+// }
+// else {
+//   output.innerHTML="password or username incorrect";
+// }
+// }
